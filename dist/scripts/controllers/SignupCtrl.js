e04ebb9992434ba240d0cefe7a9268ebf5158214
Jason Quaccia <jlquaccia@gmail.com>
2016-04-30 12:08:53 -0700

    implemented ability to register a new user

--- dist/scripts/controllers/SignupCtrl.js
+++ dist/scripts/controllers/SignupCtrl.js
@@ -1,8 +1,27 @@
 (function() {
-    function SignupCtrl() {
+    function SignupCtrl($scope, $firebaseAuth) {
+        var firebaseObj = new Firebase("https://jq-bloc-jams.firebaseIO.com");
+        var auth = $firebaseAuth(firebaseObj);
         
+        $scope.signUp = function() {
+            if (!$scope.regForm.$invalid) {
+                var email = $scope.user.email;
+                var password = $scope.user.password;
+                
+                if (email && password) {
+                    auth.$createUser(email, password)
+                        .then(function() {
+                            // do things if success
+                            console.log('User creation success');
+                        }, function(error) {
+                            // do things if failure
+                            console.log(error);
+                        });
+                }
+            }
+        };
     }
     
     angular.module('blocJams')
-        .controller('SignupCtrl', SignupCtrl);
+        .controller('SignupCtrl', ['$scope', '$firebaseAuth', SignupCtrl]);
 })();
