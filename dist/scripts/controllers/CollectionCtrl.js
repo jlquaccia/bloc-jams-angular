3c32701d762d837e4ddea6bc71b25c6ed3c870e6
Jason Quaccia <jlquaccia@gmail.com>
2016-05-03 08:20:05 -0700

    implemented ability to logout of the application

--- dist/scripts/controllers/CollectionCtrl.js
+++ dist/scripts/controllers/CollectionCtrl.js
@@ -1,9 +1,19 @@
 (function() {
-    function CollectionCtrl($scope, Fixtures) {
+    function CollectionCtrl($scope, $rootScope, $location, $firebaseAuth, Fixtures) {
         this.albums = Fixtures.getCollection(12);
+        
+        var firebaseObj = new Firebase("https://jq-bloc-jams.firebaseIO.com");
+        var loginObj = $firebaseAuth(firebaseObj);
+        
+        $rootScope.logout = function() {
+            loginObj.$unauth();
+            console.log('logged out');
+            $location.path('/');
+            $rootScope.userEmail = false;
+        };
     }
     
     angular
         .module('blocJams')
-        .controller('CollectionCtrl', ['$scope', 'Fixtures', CollectionCtrl]);
+        .controller('CollectionCtrl', ['$scope', '$rootScope', '$location', '$firebaseAuth', 'Fixtures', CollectionCtrl]);
 })();
