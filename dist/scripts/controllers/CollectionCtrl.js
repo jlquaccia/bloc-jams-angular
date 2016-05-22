fb3f7ac258db800831ce3e76f9c5c62a83497969
Jason Quaccia <jlquaccia@gmail.com>
2016-05-22 16:40:21 -0700

    added class to distinguish which song is playing on current playlist.  added some rootscope methods to profile and collection controllers

--- dist/scripts/controllers/CollectionCtrl.js
+++ dist/scripts/controllers/CollectionCtrl.js
@@ -60,6 +60,28 @@
                 }
             }
         };
+        
+        $rootScope.removeSongFromPlaylist = function(index) {
+            // remove from currentPlaylist
+            $rootScope.currentPlaylist.splice(index, 1);
+            
+            // remove from localStorage
+            var json = JSON.parse(localStorage["currentPlaylist"]);
+            
+            json.splice(index, 1);
+            localStorage["currentPlaylist"] = JSON.stringify(json);
+        };
+        
+        $rootScope.clearAllFromPlaylist = function() {
+            var confirmation = confirm("This will clear all songs from your playlist.  Are you sure?");
+            
+            if (confirmation) {
+                $rootScope.currentPlaylist = [];
+                localStorage.removeItem("currentPlaylist");
+            } else {
+                return;
+            }
+        };
     }
     
     angular
