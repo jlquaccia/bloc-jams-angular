eea27ae164da56fe3e1bb07bdc8863eaee304745
Jason Quaccia <jlquaccia@gmail.com>
2016-05-31 18:01:23 -0700

    added a semicolon or two

--- dist/scripts/controllers/StarRatingCtrl.js
+++ dist/scripts/controllers/StarRatingCtrl.js
@@ -3,7 +3,7 @@
         var ratingRef = new Firebase("https://jq-bloc-jams.firebaseIO.com/ratings" + "/" + $scope.type);
         
         $firebaseObject(ratingRef).$loaded(function(data){
-            $scope.starRatings = data
+            $scope.starRatings = data;
             $scope.score = $scope.starRatings[$scope.name];
         });
         
@@ -21,13 +21,13 @@
 
         $scope.$watch('score', function(newValue, oldValue) {
             if (newValue !== null && newValue !== undefined) {
-                key = $scope.name
-                value = newValue
+                key = $scope.name;
+                value = newValue;
                 $scope.updateStars();
                 
                 if($scope.starRatings){
                     $scope.starRatings[key] = value;
-                    $scope.starRatings.$save()
+                    $scope.starRatings.$save();
                 }
             }
         });
